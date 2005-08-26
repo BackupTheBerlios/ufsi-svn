918baafbb9602f935ed543bbd37e5fe103a12306
thekooldude <thekooldude@2d384c08-25fb-0310-ab61-b5b2a23e79ab>
2005-08-26 00:34:35 +0000

    changed to use the file object returned from the data socket

--- trunk/ufsi/src/ufsi/FtpFile.py
+++ trunk/ufsi/src/ufsi/FtpFile.py
@@ -1,29 +1,23 @@
 """
-Notes: There are two possible ways of reading and writing files to ftp
-- we can either cache everything on the first read call and cache
-everything until the close for writing, or try to do it at runtime. I
-think, because the ftplib module uses a file object that we have to go
-with the former, at least for writing.
-
-Therefore every transaction requires a separate connection (atm).
+An FTP implementation of ``ufsi.FileInterface``.
 """
 
 import ufsi
 
 import ftplib
 
-# TODO: experiment with urllib2 support.
-import urllib2
-
 
 class FtpFile(ufsi.FileInterface):
+    """
+    """
     def __init__(self,path):
+        """
+        """
         self.__path=path
         self.__pathStr=str(path)
-        self.__auth=None
         self.__ftpObject=None
         self.__ftpDataSocket=None
-        self.__openFileCache=None
+        self.__ftpDataFile=None
 
 
     def __str__(self):
@@ -37,91 +31,79 @@ class FtpFile(ufsi.FileInterface):
         # close any previously open file
         self.close()
 
-        # TODO: validate mode
+        # do some validation on the mode
         if mode not in ('r','rb','w','wb'):
             raise ufsi.UnsupportedOperationError(
-                    'The mode "%s" is not supported by the FTP '\
+                    'The mode "%s" is not supported by the FTP '
                     'implementation of an ufsi.File object.'%mode)
 
+        # get the various parts required to perform the open
         s=self.__path.split()
         host=s['host']
-        dirs=s['dirs']
-        fileBase=s['fileBase']
-        fileExt=s['fileExt']
-        filePath='/'.join(dirs)+(dirs and '/' or '')+fileBase
-        if fileExt is not None:
-            filePath+='.'+fileExt
-            
+        port=(s['port'] and s['port'] or 0)
+        urlPath=s['urlPath']
+        if urlPath is None or urlPath=='':
+            raise ufsi.PathNotFoundError(
+                    'FTP path "%s" contains no file path.'
+                    %self.__path)
+
+        # defaults
+        user=''
+        password=''
+        auth=self.__path.getAuthorisation()
+        if auth is not None:
+            if isinstance(auth,ufsi.UserPasswordAuthentication):
+                user=auth.getUser()
+                password=auth.getPassword()
+            else:
+                raise ufsi.UnsupportedAuthenticationError(
+                        'FTP only uses user,password authentication')
+        
+        # open the connection and log in
+        ftp=self.__ftpObject=ftplib.FTP()
+        ftp.connect(host,port)
+        ftp.login(user,password)
         if 'r' in mode:
-            # TODO: add use of authentication
-            ftp=self.__ftpObject=ftplib.FTP(host,'d232925','d35cr1p70r')
-            self.__ftpDataSocket=ftp.transfercmd('RETR '+filePath)
-
+            ftpCmd='RETR '+urlPath
         else:
-            # test for ability to create now and open and write on close
-            pass
+            ftpCmd='STOR '+urlPath
 
+        self.__ftpDataSocket=ftp.transfercmd(ftpCmd)
+        self.__ftpDataFile=self.__ftpDataSocket.makefile(mode)
 
-    def read(self,size=None):
-        if size is not None:
-            return self.__ftpDataSocket.recv(size)
-        else:
-            s=''
-            # TODO: create constant for min transfer size
-            r=self.__ftpDataSocket.recv(8192)
-            while len(r)>0:
-                s+=r
-                r=self.__ftpDataSocket.recv(8192)
-
-            return s
-        """
-        index=self.__openFileCache['index']
-        buffer=self.__openFileCache['buffer']
-
-        # if size is None read to the end of the buffer
-        if size is None:
-            return buffer[index:]
-        else:
-            return buffer[index:index+size]
-        """
+    def read(self,size=-1):
+        return self.__ftpDataFile.read(size)
 
     def readLine(self):
-        index=self.__openFileCache['index']
-        buffer=self.__openFileCache['buffer']
-        eolIndex=buffer.find(buffer,index)
-        
-        if eolIndex==-1:
-            eolIndex=len(buffer)
-        
-        self.__openFileCache['index']=eolIndex
-        return buffer[index:eolIndex]
+        return self.__ftpDataFile.readline()
 
     def readLines(self):
-        index=self.__openFileCache['index']
-        buffer=self.__openFileCache['buffer']
-        self.__openFileCache['index']=len(buffer)
-        return buffer[index:].splitlines(True)
+        return self.__ftpDataFile.readlines()
 
     def write(self,s):
-        buffer+=s
+        self.__ftpDataFile.write(s)
 
     def writeLines(self,lines):
-        buffer+=''.join(lines)
+        self.__ftpDataFile.writelines(lines)
 
     def close(self):
         """
-        if self.__openFileCache is not None:
-            if 'w' in self.__openFileCache['mode']:
-                # TODO: write file to ftp
+        Closes the FTP transfer.
 
-            # empty the cache
-            self.__openFileCache=None
+        TODO: Look into the socket module's closing methods - they
+        don't seem to close the socket. I may well be wrong, but if
+        errors start to occur - look there.
         """
-        if self.__ftpDataSocket is not None:
+        if self.__ftpDataFile is not None:
+            self.__ftpDataFile.close()
+            self.__ftpDataFile=None
+
             self.__ftpDataSocket.close()
-            self.__ftpObject.voidresp()
             self.__ftpDataSocket=None
+            
+            self.__ftpObject.voidresp()
             self.__ftpObject.quit()
+
 
     def getStat(self):
         pass
