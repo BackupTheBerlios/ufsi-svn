d1b9258de2887802cd72d9737e02dec05466284b
thekooldude <thekooldude@2d384c08-25fb-0310-ab61-b5b2a23e79ab>
2005-08-28 06:20:43 +0000

    completed the windows imlementation

--- trunk/ufsi/src/ufsi/NativeWindowsPath.py
+++ trunk/ufsi/src/ufsi/NativeWindowsPath.py
@@ -6,6 +6,7 @@
 import ufsi
 
 import os
+import re
 
 class NativeWindowsPath(ufsi.AbstractNativePath):
     """
@@ -17,6 +18,8 @@ class NativeWindowsPath(ufsi.AbstractNativePath):
     def __init__(self,path):
         """
         """
+        # windows recognises '/' characters (but we'll be correct)
+        path=path.replace('/','\\')
         self.__super.__init__(self,path)
         self.__split=None
 
@@ -29,8 +32,43 @@ class NativeWindowsPath(ufsi.AbstractNativePath):
             return self.__split.copy()
 
         # else split it and cache it
-        
+        # format [C:][\]dir\file.ext
+
+        p=self._path
+
+        # defaults
+        drive=None
+        dirs=[]
+        fileBase=''
+        fileExt=None
+
+        # drive
+        if re.match('[A-Za-z]:',p):
+            drive=p[0]
+            p=p[2:]
+
+        # dirs
+        dirs=p.split('\\')
+        fileBase=dirs.pop()
+        if '.' in fileBase:
+            (fileBase,fileExt)=fileBase.rsplit('.',1)
+
+        # create the dict, cache it and return a copy
+        d={}
+        d['drive']=drive
+        d['dirs']=dirs
+        d['fileBase']=fileBase
+        d['fileExt']=fileExt
+        self.__split=d
+        return d.copy()
+
 
     def getSeparator(self):
+        """
+        Returns the standard Windows dir separator of '\\'. A path on
+        windows may also be created using '/' characters as Windows
+        does recognise them, but all '/' characters are converted to
+        '\\' characters on creation of a Path object.
+        """
         return '\\'
 
