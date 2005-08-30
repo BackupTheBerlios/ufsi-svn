21e5c445027532b9814cee747c54b76abb97881a
thekooldude <thekooldude@2d384c08-25fb-0310-ab61-b5b2a23e79ab>
2005-08-30 13:22:07 +0000

    Test passing Tar implementation

--- trunk/ufsi/src/ufsi/TarFile.py
+++ trunk/ufsi/src/ufsi/TarFile.py
@@ -31,19 +31,29 @@ class TarFile(ufsi.FileInterface):
     def open(self,mode='r'):
         """
         """
-        if mode not in ['r']:
-            raise ufsi.InvalidArgumentError('mode must be "r"')
+        if mode in ['r','rb']:
+            tarFile=self.__path.getTarFilePath()
+            tf=tarFile.getFile()
+            tf.open('r')
+            self.__tarFileObject=tar=tarfile.TarFile('','r',tf)
+            try:
+                self.__fileHandle=tar.extractfile(self.__tarPathStr)
+            except KeyError,e:
+                raise ufsi.PathNotFoundError('Path "%s" not found'
+                                             %self.__tarPathStr,e)
 
-        tarFile=self.__path.getTarFilePath()
-        tf=tarFile.getFile()
-        tf.open('r')
-        self.__tarFileObject=tar=tarfile.TarFile('','r',tf)
-        self.__fileHandle=tar.extractfile(self.__tarPathStr)
-
-        return self.__fileHandle
+        elif mode in ['w','wb','a','ab']:
+            raise ufsi.UnsupportedOperationError("Write is not supported")
+        else:
+            raise ufsi.InvalidArgumentError('mode must be "r" or "rb"')
 
     def read(self,size=-1):
-        return self.__fileHandle.read(size)
+        # TODO: apparently tarfile won't take -1 as a size
+        if size<0:
+            s=self.__fileHandle.read()
+        else:
+            s=self.__fileHandle.read(size)
+        return s
 
     def readLine(self):
         return self.__fileHandle.readline()
@@ -52,10 +62,10 @@ class TarFile(ufsi.FileInterface):
         return self.__fileHandle.readlines()
 
     def write(self,s):
-        raise UnsupportedOperationError('Tar files can only be read.')
+        raise ufsi.UnsupportedOperationError('Tar files can only be read.')
 
     def writeLines(self,lines):
-        raise UnsupportedOperationError('Tar files can only be read.')
+        raise ufsi.UnsupportedOperationError('Tar files can only be read.')
 
     def close(self):
         if self.__fileHandle is not None:
