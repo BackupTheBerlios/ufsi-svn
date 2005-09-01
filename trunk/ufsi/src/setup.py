bfa21bff1065f707240c220b241328b13311c6e7
thekooldude <thekooldude@2d384c08-25fb-0310-ab61-b5b2a23e79ab>
2005-09-01 23:43:41 +0000

    initial basic installation script

---
+++ trunk/ufsi/src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="ufsi",
+      version="0.1",
+      description="Python Universal File System Interface and Implementations",
+      packages=["ufsi"],
+      author="Adam Kerz",
+      author_email="adamkerz@adamkerz.com"
+      )
